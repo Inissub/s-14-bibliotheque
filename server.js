6780897e2fb4dfe6a87bd478d509b08fe50a53ad
Inissub <bussinibsk@gmail.com>
2026-09-10 19:32:10 +0200

    feat: dossier config/database.js et server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pool = require('./config/database');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('Hello Moto')
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
